d28db6eaf1c0d31f1ae035357b7dc6f3c2f05a2b
Yuliana Alvarado <yalvarado924@gmail.com>
2021-02-02 17:48:40 -0600

    commit tree de9a46ed62544392596599ed2deeddb5c745e1a4

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Yuli"
+    end
+
+    get '/hometown' do
+        "My hometown is Harvard"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is My Shot"
+    end
 end
